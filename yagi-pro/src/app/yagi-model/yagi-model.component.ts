83f50849b032dd5e50ee8def6c812e5051faa02c
Norkator <55850510+norkator@users.noreply.github.com>
2020-06-01 15:18:55 +0300

    Working visualization

--- yagi-pro/src/app/yagi-model/yagi-model.component.ts
+++ yagi-pro/src/app/yagi-model/yagi-model.component.ts
@@ -1,5 +1,5 @@
-import {Component, Input, OnInit} from '@angular/core';
-import {YagiInterface} from "../interfaces";
+import {Component, ElementRef, Input, OnInit, ViewChild} from '@angular/core';
+import {YagiElementInterface, YagiInterface} from "../interfaces";
 
 @Component({
   selector: 'app-yagi-model',
@@ -10,23 +10,56 @@ export class YagiModelComponent implements OnInit {
 
   @Input('yagi') public yagi = <YagiInterface>{};
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
 
 
   ngAfterViewInit() {
-
   }
 
 
-  public getViewAspectWidth(elementWidth: number): number {
-    return elementWidth * 3.7795275591 / 20;
+  public getElementStyle(element: YagiElementInterface): any {
+    return {
+      'width': this.calculateWidthPercentage(element) + '%',
+      'height': '10px',
+      'background-color': '#a2a3a4'
+    }
   }
 
-  public getElementDistanceMillis(position: number, distance: number): number {
-    return distance === null ? position : distance;
+
+  public getBoomStyle(element: YagiElementInterface): any {
+    let h = (element.distance === null ? element.position : element.distance)  / 2;
+    if (h <= 0) {
+      h = 40;
+    }
+
+    return {
+      'width': '2%',
+      'height':  h + 'px',
+      'background-color': '#c2c3c4'
+    }
   }
+
+
+  /**
+   * Find longest, use longest as reference
+   * to calculate suitable length for others
+   * @param element Yagi elements
+   */
+  private calculateWidthPercentage(element: YagiElementInterface): number {
+    const longest = this.yagi.yagiElements.reduce(function (previous, current) {
+      return (previous.length > current.length) ? previous : current;
+    }).length;
+
+    // noinspection UnnecessaryLocalVariableJS
+    const p = 100 - Math.floor(((longest - element.length) / ((longest + element.length) / 2)) * 100);
+    // console.info(longest, element.length, p);
+
+    return p;
+  }
+
 
 }
